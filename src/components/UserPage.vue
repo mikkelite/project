f4c3cef0a432eef8e1ee0bec116593c00a4852a7
mikkelite <michael.felker@ymail.com>
2024-04-09 19:35:19 -0700

    user page made with css

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,17 +1,17 @@
 <template>
     <h1>Account Creation</h1>  
-        <form>     
-            <br />
+        <form style="display: grid">     
+       
             <label>Enter First Name</label>
             <input type="text" required>
-            <br />
+       
             <label>Enter Last Name</label>
             <input type="text" required>    
-            <br />
+     
             <label>Enter Email Address</label> 
             <input type="email"  required>
-            <br />
-            <button @click="CreateUser" type="submit">Create User</button>
+      
+            <button  @click="CreateUser" type="submit">Create User</button>
         </form>
 </template>
 <script>
@@ -35,3 +35,54 @@ export default{
 
 }
 </script>
+<style>
+body {
+  font-family: Arial, sans-serif; 
+  margin: 0; 
+}
+label{
+    margin-left: 10px;
+}
+p{
+    display: inline-flex;
+    padding:10px
+}
+button{
+    width: fit-content
+}
+input{
+    display: inline-block;
+    margin: auto;
+    box-sizing: border-box;
+    margin: 10px;
+    width: fit-content
+
+}
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0; 
+}
+
+li {
+  display: flex; 
+  margin: 10px; 
+  padding: 10px; 
+  border: 1px solid #ddd; 
+  border-radius: 5px; 
+}
+
+li a {
+  text-decoration: none;
+  color: #000;
+}
+
+li a:hover {
+  color: #333; 
+}
+
+
+li:hover {
+  background-color: #eee; 
+}</style>
+
