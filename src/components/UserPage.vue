d66566d5d1a94a0b625fe39e038cf4146870a543
mikkelite <michael.felker@ymail.com>
2024-04-09 19:20:29 -0700

    fixed errors

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,18 +1,18 @@
 <template>
-<form>
-    <h1>Account Creation</h1>      
-        <br />
-        <label>Enter First Name</label>
-        <input v-model="newProduct.price" required>
-        <br />
-        <label>Enter Last Name</label>
-        <input v-model="newProduct.NumberInStock" required>    
-        <br />
-        <label>Enter Email Address</label> 
-        <input type="email" v-model= required>
-        <br />
-        <button @click="CreateUser" type="submit">Create User</button>
-    </form>
+    <h1>Account Creation</h1>  
+        <form>     
+            <br />
+            <label>Enter First Name</label>
+            <input type="text" required>
+            <br />
+            <label>Enter Last Name</label>
+            <input type="text" required>    
+            <br />
+            <label>Enter Email Address</label> 
+            <input type="email"  required>
+            <br />
+            <button @click="CreateUser" type="submit">Create User</button>
+        </form>
 </template>
 <script>
 export default{
